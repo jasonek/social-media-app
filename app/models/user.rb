f8ca4eff40b8008a773af1da4b1627b62c3c31a2
Jason Kucharski <jasonok@protonmail.ch>
2017-01-05 14:41:45 -0800

    add password and bcrypt to User model, and associated migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-   validates :name, presence: true, length: { maximum: 50 }
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
 
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d]+[a-z\d\-.]+\.[a-z]+\z/i
-   validates :email, presence: true, length: { maximum: 250 }, format: { with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 250 }, format: { with: VALID_EMAIL_REGEX }
 
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.create(email: 'jason@jason.com', name: 'Jason')
+    @user = User.new(email: 'jason@jason.com', name: 'Jason', password: 'abc123', password_confirmation: 'abc123')
   end
 
   test '@user should be valid' do
@@ -40,10 +40,37 @@ class UserTest < ActiveSupport::TestCase
 
   test 'email validation should reject invalid addresses' do
     invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com)
+                           foo@bar_baz.com foo@bar+baz.com foo@.bar.com foo@-bar.com foo@_bar.com foo@bar..com)
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email should be downcased before save" do
+    newemail = "WoWLooKaTMe@test.com"
+    @user.email = newemail
+    @user.save
+    assert_equal newemail.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+
+
 end
