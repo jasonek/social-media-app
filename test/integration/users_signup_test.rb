d1479992ed5350de1a215713ed6aea89068e7ddc
Jason Kucharski <jasonok@protonmail.ch>
2017-01-16 10:27:53 -0800

    create valid user integration test and use content_tag helper method

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,7 +8,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: {email: "user@invalid.com", password: 'abc123', password_confirmation: 'WRONG'}}
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Barry", email: "user@valid.com", password: 'abc123', password_confirmation: 'abc123'}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.danger
+  end
+
 end
