0951fbd447917295b21e8c82ac85388d9092dee0
Jason Kucharski <jasonok@protonmail.ch>
2017-01-16 10:02:16 -0800

    create integration test and name signup route

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {email: "user@invalid.com", password: 'abc123', password_confirmation: 'WRONG'}}
+    end
+    assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+  end
+end
